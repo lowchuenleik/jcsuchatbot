af2590247c0172f5a3b559b4cbf4a2df98d248c5
Chuen Leik <chuenleik_3837@hotmail.com>
2019-09-29 00:24:24 +0100

    removed refactoring

--- index.js
+++ index.js
@@ -28,7 +28,96 @@ function handlePostback(sender_psid, received_postback) {
   // Send the message to acknowledge the postback
   handler.callSendAPI(sender_psid, response);
 }
+function handleMessage(sender_psid, received_message) {
 
+  let response;
+
+  const greeting = firstEntity(message.nlp, 'meal');
+
+  console.log("OMG LOOK HERE for GREETING",)
+  response = {
+  "text": `You sent the message: "${received_message.text}". Now send me an image! - Chuen`
+  }
+  // check greeting is here and is confident
+  // if (greeting && greeting.confidence > 0.8) {
+  // // } else { 
+  // //     // default logic
+  // // }
+
+  // // // Check if the message contains text
+  // // if (received_message.text) {    
+      
+  // //     let message= received_message.text;
+
+ 
+  //   // Create the payload for a basic text message
+  //   console.log("OMG LOOK HERE",)
+  //   response = {
+  //     "text": `You sent the message: "${received_message.text}". Now send me an image! - Chuen`
+  //   }
+  // }  
+  // else if (received_message.attachments) {
+
+  //   // Gets the URL of the message attachment
+  //   let attachment_url = received_message.attachments[0].payload.url;
+  //   response = {
+  //     "attachment": {
+  //       "type": "template",
+  //       "payload": {
+  //         "template_type": "generic",
+  //         "elements": [{
+  //           "title": "Is this the right picture?",
+  //           "subtitle": "Tap a button to answer.",
+  //           "image_url": attachment_url,
+  //           "buttons": [
+  //             {
+  //               "type": "postback",
+  //               "title": "Yes!",
+  //               "payload": "yes",
+  //             },
+  //             {
+  //               "type": "postback",
+  //               "title": "No!",
+  //               "payload": "no",
+  //             }
+  //           ],
+  //         }]
+  //       }
+  //     }
+  //   }
+  
+  // } 
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+
+}
+
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+    "recipient": {
+        "id": sender_psid
+    },
+    "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+        console.log('message sent!')
+        } else {
+        console.error("Unable to send message:" + err);
+        }
+    }); 
+}
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
